75d139cee8b598da4eb59708a2ca0884ae9526a4
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-06-03 18:05:09 -0500

    client setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Line } from 'react-chartjs-2';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js';
+import Plot from 'react-plotly.js';
 
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 function App() {
   const [showActualData, setShowActualData] = useState(false);
   const [showStats, setShowStats] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([{ x: 1, y: 2 }, { x: 2, y: 3 }, { x: 1, y: 4 }]);
   const [line, setLine] = useState({ m: 1, b: 0 });
 
   const handleRandomize = () => {
@@ -54,6 +55,9 @@ function App() {
         label: 'User Data',
         data: showActualData ? data : [],
         backgroundColor: 'rgba(75, 192, 192, 1)',
+        type: 'scatter',
+        pointRadius: 6,
+
       },
       {
         label: 'Regression Line',
@@ -70,6 +74,34 @@ function App() {
     ],
   };
 
+
+
+  const handleUpdateModelLocally = () => {
+
+
+  };
+
+  const handlePushModelToServer = () => {
+
+  };
+
+  const [client1Points, setClient1Points] = useState([]);
+  const handleClient1Plot = (event) => {
+    const { clientX, clientY } = event;
+    setClient1Points([...client1Points, { x: clientX, y: clientY }]);
+  };
+  const [client2Points, setClient2Points] = useState([]);
+  const handleClient2Plot = (event) => {
+    const { clientX, clientY } = event;
+    setClient2Points([...client2Points, { x: clientX, y: clientY }]);
+  };
+  const [client3Points, setClient3Points] = useState([]);
+  const handleClient3Plot = (event) => {
+    const { clientX, clientY } = event;
+    setClient3Points([...client3Points, { x: clientX, y: clientY }]);
+  };
+
+
   const options = {
     scales: {
       x: {
@@ -81,9 +113,9 @@ function App() {
 
   return (
     <div className='flex flex-col p-10'>
-      <h1 className="text-2xl font-bold mb-4">Federated Learning with Blockchain Incentives Demo</h1>
+      <h1 className="text-2xl font-bold mb-4">Federated Learning Demo</h1>
 
-      <div className="border p-4">
+      <div className="border p-4 bg-slate-300">
         <h2 className="text-xl font-bold mb-4"> Centeral Server Model </h2>
 
         <div className="flex justify-between mb-2">
@@ -91,7 +123,7 @@ function App() {
           <button onClick={handleRandomize} className="bg-blue-500 text-white px-2 py-1 rounded">Randomize Model</button>
         </div>
         <Line data={scatterData} options={options} />
-        <div className="flex mt-4">
+        <div className="flex mt-4 p-4 bg-blue-500">
           <div className="mr-4">
             <input
               type="checkbox"
@@ -121,10 +153,56 @@ function App() {
         )}
       </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <div className="grid grid-cols-3 mt-4">
-        <div className='border p-4'>1</div>
-        <div className='border p-4'>2</div>
-        <div className='border p-4'>3</div>
+
+
+        <div className='border-2 p-4'>
+
+          <h2 className="text-xl font-bold mb-4">Client 1</h2>
+
+          <button onClick={() => handleUpdateModelLocally()} className="bg-yellow-500 text-white px-4 py-2 rounded mt-4">Update Model Locally</button>
+          <button onClick={() => handlePushModelToServer()} className="bg-red-500 text-white px-4 py-2 rounded mt-2">Push Model to Server</button>
+        </div>
+
+
+
+
+
+
+        <div className='border-2 p-4'>
+
+          <h2 className="text-xl font-bold mb-4">Client 2</h2>
+
+          <button onClick={() => handleUpdateModelLocally()} className="bg-yellow-500 text-white px-4 py-2 rounded mt-4">Update Model Locally</button>
+          <button onClick={() => handlePushModelToServer()} className="bg-red-500 text-white px-4 py-2 rounded mt-2">Push Model to Server</button>
+        </div>
+
+
+
+
+
+        <div className='border-2 p-4'>
+
+          <h2 className="text-xl font-bold mb-4">Client 3</h2>
+
+          <button onClick={() => handleUpdateModelLocally()} className="bg-yellow-500 text-white px-4 py-2 rounded mt-4">Update Model Locally</button>
+          <button onClick={() => handlePushModelToServer()} className="bg-red-500 text-white px-4 py-2 rounded mt-2">Push Model to Server</button>
+        </div>
+
       </div>
 
       <ToastContainer />
